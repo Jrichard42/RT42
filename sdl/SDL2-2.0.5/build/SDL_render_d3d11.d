23cc041b659a7692266b42c98a362d49b18d5286
dbreton <dbreton@student.42.fr>
2017-02-20 14:44:52 +0100

    fixed sdl install

--- sdl/SDL2-2.0.5/build/SDL_render_d3d11.d
+++
@@ -1,7 +0,0 @@
-build/SDL_render_d3d11.lo: \
-  /Users/abitoun/rt42/sdl/SDL2-2.0.5/src/render/direct3d11/SDL_render_d3d11.c \
-  /Users/abitoun/rt42/sdl/SDL2-2.0.5/src/render/direct3d11/../../SDL_internal.h \
-  /Users/abitoun/rt42/sdl/SDL2-2.0.5/src/render/direct3d11/../../dynapi/SDL_dynapi.h \
-  /Users/abitoun/rt42/sdl/SDL2-2.0.5/src/render/direct3d11/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
