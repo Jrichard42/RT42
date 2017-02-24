29e96c95cc30e4d0d8e6c23a99065c2d3abd036a
Hugo PACHY <hpachy@e1r12p6.42.fr>
2017-02-24 15:38:32 +0100

    main.c fixed

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hpachy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 16:26:17 by hpachy            #+#    #+#             */
-/*   Updated: 2017/02/18 19:40:41 by jrichard         ###   ########.fr       */
+/*   Updated: 2017/02/24 15:37:22 by hpachy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int				main(int argc, char **argv)
 
 	if (argc != 2)
 		return (-1);
-	rt = create_rt(WIN_X, WIN_X, argv[1]);
+	rt = create_rt(WIN_X, WIN_Y, argv[1]);
 	loop(rt);
 	// fontion destruction en attente
 	return (0);
