a82765af31b20f949760a804d3238e533aa91cb0
Drien BRETON <drien.breton@gmail.com>
2017-02-27 10:36:11 +0100

    added hashtable header and skeleton source file

---
+++ srcs/hashtable.c
@@ -0,0 +1,15 @@
+//
+// Created by dbreton on 2/27/17.
+//
+
+#include "hashtable.h"
+t_hashtable     *create_hash_table(size_t size)
+{}
+static size_t          hash(t_hashtable *table, char *key)
+{}
+static t_entry         *create_pair(char *key, void *value)
+{}
+void            ht_set(t_hashtable *table, char *key, void *value)
+{}
+void            *ht_get(t_hashtable *table, char *key)
+{}
