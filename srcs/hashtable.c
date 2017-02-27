42a59a4b82866228fc3e930f4d7a28a60dbb25bd
Drien BRETON <drien.breton@gmail.com>
2017-02-27 11:43:42 +0100

    added getter and setter function

--- srcs/hashtable.c
+++ srcs/hashtable.c
@@ -12,9 +12,9 @@ t_hashtable     *create_hash_table(size_t size)
     if (size == 0)
         return NULL;
     if (!(table = (t_hashtable *)malloc(sizeof(t_hashtable))))
-        return NULL;
+        return (NULL);
     if (!(table->table = (t_entry *)malloc(sizeof(t_entry) * size)))
-        return NULL;
+        return (NULL);
     while (i < size)
     {
         table->table[i] = NULL;
@@ -39,8 +39,34 @@ static size_t          hash(t_hashtable *table, char *key)
     return (hashval % table->size);
 }
 static t_entry         *create_pair(char *key, void *value)
-{}
+{
+    t_entry             *new_pair;
+
+    if (!(new_pair = (t_entry *)malloc(sizeof(t_entry))))
+        return (NULL);
+    if (!(new_pair->key = ft_strdup(key)))
+        return (NULL);
+    new_pair->value = value;
+    return (new_pair);
+}
 void            ht_set(t_hashtable *table, char *key, void *value)
-{}
+{
+    size_t      bin;
+    t_entry     *new_pair;
+
+    bin = hash(table, key);
+    new_pair = create_pair(key, value);
+    if (new_pair)
+        table->table[bin] = new_pair;
+}
 void            *ht_get(t_hashtable *table, char *key)
-{}
+{
+    size_t      bin;
+    t_entry     *pair;
+
+    bin = hash(table, key);
+    if (!pair || !pair->key)
+        return (NULL);
+    else
+        return (pair->value);
+}
