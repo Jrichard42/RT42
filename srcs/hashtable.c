718bb8dc02e50a0a10301f82c267c5f0972f29ff
Drien BRETON <drien.breton@gmail.com>
2017-02-27 11:00:14 +0100

    added hashing function

--- srcs/hashtable.c
+++ srcs/hashtable.c
@@ -25,7 +25,19 @@ t_hashtable     *create_hash_table(size_t size)
 
 }
 static size_t          hash(t_hashtable *table, char *key)
-{}
+{
+    size_t              hashval;
+    int                 i;
+
+    i = 0;
+    while (hashval <ULONG_MAX && key[i] != '\0')
+    {
+        hashval = hashval << 8;
+        hashval += key[i];
+        ++i;
+    }
+    return (hashval % table->size);
+}
 static t_entry         *create_pair(char *key, void *value)
 {}
 void            ht_set(t_hashtable *table, char *key, void *value)
