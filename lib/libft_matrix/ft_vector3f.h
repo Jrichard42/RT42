36dbc214508642e6f10434acb7ae5aa8271fb6b5
Jeremy RICHARD <jrichard@e1r11p16.42.fr>
2017-03-13 14:04:16 +0100

    norme error

--- lib/libft_matrix/ft_vector3f.h
+++ lib/libft_matrix/ft_vector3f.h
@@ -6,7 +6,7 @@
 /*   By: jrichard <jrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 14:36:23 by jrichard          #+#    #+#             */
-/*   Updated: 2017/02/25 13:11:26 by jrichard         ###   ########.fr       */
+/*   Updated: 2017/03/13 14:03:45 by jrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ t_vector3f		normalize_vector3f(t_vector3f v1);
 float			length_vector3f(t_vector3f v1);
 float			squared_length_vector3f(t_vector3f v1);
 t_vector3f		transform_vector3f(t_vector3f v1, t_matrix *transf);
-t_vector3f		clamp_vector3f(t_vector3f, float min, float max);
+t_vector3f		clamp_vector3f(t_vector3f v1, float min, float max);
 t_vector3f		mult_by_vector3f(t_vector3f v1, t_vector3f v2);
 t_vector3f		div_float_by_vector3f(float nb, t_vector3f v);
 
