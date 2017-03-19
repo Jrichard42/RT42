769c94fb3d44d46d84a461fc6c1ca60c506321bf
Jeremy RICHARD <jrichard@e1r3p1.42.fr>
2017-03-19 18:35:59 +0100

    clean

--- sdl/SDL2-2.0.5/build/.libs/SDL_rwopsbundlesupport.d
+++
@@ -1,8 +0,0 @@
-build/SDL_rwopsbundlesupport.lo: \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/file/cocoa/SDL_rwopsbundlesupport.m \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/file/cocoa/../../SDL_internal.h \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/file/cocoa/../../dynapi/SDL_dynapi.h \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/file/cocoa/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/file/cocoa/SDL_rwopsbundlesupport.h
--- sdl/SDL2-2.0.5/build/SDL_malloc.d
+++
@@ -1,7 +0,0 @@
-build/SDL_malloc.lo: \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/stdlib/SDL_malloc.c \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/stdlib/../SDL_internal.h \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/jrichard/Projects/RT/rt42/sdl/SDL2-2.0.5/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h
