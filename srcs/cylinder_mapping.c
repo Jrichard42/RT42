fbc2cfd6af58c75f1ac54094529f9f302bbdcf5c
dbreton <dbreton@student.42.fr>
2017-03-20 12:15:57 +0100

    first draft of plane texture mapping

--- srcs/cylinder_mapping.c
+++ srcs/cylinder_mapping.c
@@ -17,20 +17,25 @@
 #include "obj.h"
 #include "inter.h"
 
-t_vector3f			cylinder_mapping(t_vector3f inter_obj, t_vector3f **texture)
+t_vector3f			cylinder_mapping(t_inter inter_obj, t_vector3f **texture)
 {
 	t_vector3f		color;
+	t_vector3f		pos;
 	static	int		tmp;
 
 	color = create_vector3f(0, 0, 0);
-	if (inter_obj.x < 0)
+	pos = sub_vector3f(inter_obj.obj->pos, inter_obj.impact);
+	pos.x = fabsf(pos.x);
+	pos.y = fabsf(pos.y);
+	pos.z = fabsf(pos.z);
+	/*if (inter_obj.x < 0)
 		inter_obj.x = -inter_obj.x;
 	if (inter_obj.y < 0)
 		inter_obj.y = -inter_obj.y;
 	if (inter_obj.x < 1)
 		inter_obj.x = 1;
 	if (inter_obj.y < 1)
-		inter_obj.y = 1;
+		inter_obj.y = 1;*/
 	if (tmp < 100)
 	{
 		tmp++;
@@ -38,8 +43,9 @@ t_vector3f			cylinder_mapping(t_vector3f inter_obj, t_vector3f **texture)
 		color.y = color.z = 0;
 		return (color);
 	}
-	printf("%f|%f\n", inter_obj.x, inter_obj.y);
-	color = texture[(int)(inter_obj.y)][(int)(inter_obj.x)];
+	//printf("%f|%f\n", inter_obj.impact.x, inter_obj.impact.y);
+	//color = texture[(int)(abs(inter_obj.impact.y - inter_obj.obj->pos.y))][(int)abs((inter_obj.impact.x - inter_obj.obj->pos.x))];
+	color = texture[(int)pos.y][(int)pos.x];
 	// printf("color %f|%f|%f\n", color.x, color.y, color.z);
 	return (color);
 }
