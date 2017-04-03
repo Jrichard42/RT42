2285dff4dd791dcc1a1be2e93e4263e45af11017
dbreton <dbreton@student.42.fr>
2017-04-03 13:56:30 +0200

    fixed bus error on exit

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -106,6 +106,7 @@ int					create_cylinder(t_kvlexer *token, t_rt *rt)
 	obj.normal = &normal_cylinder;
 	obj.inter = &inter_cylinder;
 	obj.texture = &cyl_tex;
+	obj.destroy = NULL;
 	if (create_cylinder2(token, rt, &obj))
 		ft_lstadd(&rt->objs, ft_lstnew(&obj, sizeof(obj)));
 	else
