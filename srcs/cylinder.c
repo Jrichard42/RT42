3d64b1fa6a948b1c737b9e37c555e0521dc6b28f
dbreton <dbreton@student.42.fr>
2017-04-03 12:17:10 +0200

    added relatively well working cylinder texture apply

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: hpachy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 16:29:48 by hpachy            #+#    #+#             */
-/*   Updated: 2017/03/30 20:11:35 by jrichard         ###   ########.fr       */
+/*   Updated: 2017/03/24 17:50:21 by dbreton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,26 @@
 #include "cylinder.h"
 #include "obj.h"
 #include "parser.h"
-#include "libft.h"
-#include "cut.h"
 #define CYLINDER ((t_cylinder *)obj->data)
 
-static float		inter_cylinder(t_obj *obj, t_ray *ray)
+static	t_vector3f	cyl_tex(t_obj *obj, t_inter inter)
+{
+	t_vector3f		color;
+	t_vector3f		pos;
+	t_vector3f		uv;
+
+	pos = sub_vector3f(inter.impact, obj->pos);
+	uv.z = sqrtf(
+		ceilf(squared_length_vector3f(pos) - powf(CYLINDER->radius, 2)));
+	uv.y = atan2f(pos.y, pos.x);
+	if (uv.y < 0.0f)
+		uv.y += 2.0f * M_PI;
+	uv.x = (uv.z / (CYLINDER->radius * 2.0f * M_PI));
+	color = get_tex_point(obj->tex, uv.x, uv.y);
+	return (color);
+}
+
+static	float		inter_cylinder(t_obj *obj, t_ray *ray)
 {
 	t_quadratic		var;
 	t_vector3f		tmp;
@@ -42,28 +57,13 @@ static float		inter_cylinder(t_obj *obj, t_ray *ray)
 		var.result = var.sol_2;
 	else
 		var.result = var.sol_1;
-	if (CYLINDER->h != 0)
-		var.result = cut_cylinder(obj, ray, &var);
 	return (var.result);
 }
 
 static t_vector3f	normal_cylinder(struct s_obj *obj, t_vector3f *impact)
 {
 	t_vector3f		tmp;
-	float			d;
 
-	if (CYLINDER->h)
-	{
-		d = dot_vector3f(CYLINDER->dir, obj->pos);
-		if (((dot_vector3f(*impact, CYLINDER->dir) - d) < 0.0005f)
-				&& ((dot_vector3f(*impact, CYLINDER->dir) - d) > -0.0005f))
-			return (mult_vector3f(CYLINDER->dir, -1));
-		d = dot_vector3f(CYLINDER->dir, add_vector3f(obj->pos,
-			mult_vector3f(CYLINDER->dir, CYLINDER->h)));
-		if (((dot_vector3f(*impact, CYLINDER->dir) - d) < 0.0005f)
-				&& ((dot_vector3f(*impact, CYLINDER->dir) - d) > -0.0005f))
-			return (CYLINDER->dir);
-	}
 	tmp = sub_vector3f(*impact, obj->pos);
 	tmp = mult_vector3f(CYLINDER->dir, dot_vector3f(tmp, CYLINDER->dir));
 	tmp = add_vector3f(tmp, obj->pos);
@@ -93,8 +93,6 @@ static int			create_cylinder2(t_kvlexer *token, t_rt *rt, t_obj *obj)
 		return ((int)ft_error("The CYLINDER should contain a field RADIUS"));
 	if (!get_as_vector3f(token, "DIR", &(CYLINDER->dir)))
 		return ((int)ft_error("The CYLINDER should contain a field DIR"));
-	if (!get_as_float(token, "HAUTEUR", &(CYLINDER->h)))
-		CYLINDER->h = 0;
 	CYLINDER->dir = normalize_vector3f(CYLINDER->dir);
 	return (1);
 }
@@ -107,8 +105,7 @@ int					create_cylinder(t_kvlexer *token, t_rt *rt)
 		return (0);
 	obj.normal = &normal_cylinder;
 	obj.inter = &inter_cylinder;
-	obj.texture = NULL;
-	obj.destroy = NULL;
+	obj.texture = &cyl_tex;
 	if (create_cylinder2(token, rt, &obj))
 		ft_lstadd(&rt->objs, ft_lstnew(&obj, sizeof(obj)));
 	else
